7f101879e39bb5725ada8302dd2f48b2ced41111
Gilberto Barbosa <gisantos555@gmail.com>
2022-09-09 12:57:36 -0300

    Capítulo 1

---
+++ src/Aluno.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+class Alura
+{
+    private CPF $cpf;
+    private string $nome;
+    private Email $email;
+}
+
---
+++ src/Email.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+use Stringable;
+
+class Email implements Stringable
+{
+    private string $endereco;
+
+    public function __construct(string $endereco)
+    {
+        if (filter_var($endereco, FILTER_VALIDATE_EMAIL) === false){
+            throw new \InvalidArgumentException(
+                'Endereço de e-mail inválido'
+            );
+        }
+        
+        $this->endereco = $endereco;
+    }
+
+    public function __toString(): string
+    {
+        return $this->endereco;
+    }
+}
